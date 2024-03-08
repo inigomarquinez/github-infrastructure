19ffbe6f55c79cc23b16d42d278b73884894ab65
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2024-03-08 21:32:50 +0100

    fix: required_providers definition

--- repositories/templates/version.tf
+++ repositories/templates/version.tf
@@ -1,10 +1,10 @@
-# terraform {
-#   required_version = ">= 1.6"
+terraform {
+  required_version = ">= 1.6"
 
-#   required_providers {
-#     github = {
-#       source  = "integrations/github"
-#       version = "6.0.1"
-#     }
-#   }
-# }
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "6.0.1"
+    }
+  }
+}
