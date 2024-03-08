86aef78832781d01392a8100572b30f08b921a59
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2024-03-08 15:19:13 +0100

    fix: remove variables

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 # GitHub provider authentication.
 # 🔗 https://registry.terraform.io/providers/integrations/github/latest/docs#oauth--personal-access-token
-provider "github" {
-  token = var.github_token
-  owner = var.github_token_owner
-}
+# provider "github" {
+#   token = var.github_token
+#   owner = var.github_token_owner
+# }
 
 module "repositories" {
   source = "./repositories"
--- variables.tf
+++ variables.tf
@@ -2,12 +2,12 @@
 # Required variables.
 # -----------------------------------------------------------------------------
 
-variable "github_token" {
-  description = "Github token"
-  type        = string
-}
+# variable "github_token" {
+#   description = "Github token"
+#   type        = string
+# }
 
-variable "github_token_owner" {
-  description = "Github token owner"
-  type        = string
-}
+# variable "github_token_owner" {
+#   description = "Github token owner"
+#   type        = string
+# }
