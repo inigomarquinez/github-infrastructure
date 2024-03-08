979003d4a65e680a4e23094abb714d7eaa6e63d9
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2024-03-08 16:35:07 +0100

    chore: set archive_on_destroy to false for test-repo (#2)

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-# GitHub provider authentication.
-# 🔗 https://registry.terraform.io/providers/integrations/github/latest/docs#oauth--personal-access-token
-# provider "github" {
-#   token = var.github_token
-#   owner = var.github_token_owner
-# }
-
 module "repositories" {
   source = "./repositories"
 }
--- repositories/main.tf
+++ repositories/main.tf
@@ -6,7 +6,8 @@ module "repository-test-repo" {
   source = "./templates"
 
   github_repository = {
-    name      = "test-repo",
-    topics    = ["terraform", "github", "test"]
+    name                = "test-repo",
+    topics              = ["terraform", "github", "test"],
+    archive_on_destroy  = false
   }
 }
